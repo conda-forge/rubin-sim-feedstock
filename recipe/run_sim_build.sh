d0b6e88509417a874d2ae83f5ab6af1b0049fbea
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-04-17 13:25:11 -0500

    Update run_sim_build.sh

--- recipe/run_sim_build.sh
+++ recipe/run_sim_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "version: ${PKG_VERSION}"
-SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION} ${PYTHON} -m pip install . -vv
+SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION} ${PYTHON} -m pip install . -vv --no-build-isolation
